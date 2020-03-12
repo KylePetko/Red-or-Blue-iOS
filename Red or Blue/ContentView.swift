31e77f02e2acb92288592c2d181f6b9569c00fd7
Kyle <kyle@kylepet.co>
2020-03-11 20:04:45 -0700

    It actually works now

--- Red or Blue/ContentView.swift
+++ Red or Blue/ContentView.swift
@@ -17,8 +17,9 @@ struct ContentView: View {
     
     
     @ObservedObject var day = GetDay()
-    let url = NSURL(string: "redorblue.kylepet.co")
+    
     var body: some View {
+        
         
         
             NavigationView {
@@ -38,7 +39,10 @@ struct ContentView: View {
                         Spacer()
                         Text("\(day.currentDay)")
                             .font(.largeTitle)
-                            //.foregroundColor(.red)
+                            .multilineTextAlignment(.center)
+                            .foregroundColor(day.textColor)
+                            .animation(Animation.spring())
+                        
                     }
                     .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
                     .edgesIgnoringSafeArea(.all)
@@ -63,17 +67,72 @@ struct ContentView: View {
        
     }
 
+
+
+
 class GetDay: ObservableObject {
-    @Published var currentDay = "0"
+    @Published var currentDay = ""
+    @Published var textColor = Color.white
 
     init() {
         
-        let url = NSURL(string: "https://www.googleapis.com/calendar/v3/calendars/lgsuhsd.org_tebqf0pqvog3p4s5flsmddg4u8%40group.calendar.google.com/events?maxResults=3&key=AIzaSyCIQTEHeJgecY8GIZrHBnbgFWu3enzr304&futureevents=true&singleevents=true")
+        //Calculate date
+        let today = Date()
+        let formatter = ISO8601DateFormatter()
+        let timeFromGMT = -1 * 60 * 60 * 7
+        formatter.timeZone = TimeZone(secondsFromGMT: timeFromGMT)
+        formatter.formatOptions.insert(.withFractionalSeconds)
+        var modifiedDate = Calendar.current.date(byAdding: .day, value: 1, to: today)!
+        
+        
+        let dateText = formatter.string(from: today)
+        
+        var modifiedDateBeforeText = formatter.string(from: modifiedDate)
+        
+        
+        var beforeDate = modifiedDateBeforeText.prefix(10)
+       
+        
+        
+        
+        let url = NSURL(string: "https://www.googleapis.com/calendar/v3/calendars/lgsuhsd.org_tebqf0pqvog3p4s5flsmddg4u8%40group.calendar.google.com/events?&key=AIzaSyB9LL_hsg4zvXzMhUATBCXp92P5Q7LHxe0&timeMin=\(beforeDate)T10:00:00-07:00&singleEvents=true&orderBy=startTime&timeMax=\(beforeDate)T10:01:00-07:00")
+        
         let task = URLSession.shared.dataTask(with: url! as URL) {(data, response, error) in
             let dataAsNSString = NSString(data: data!, encoding: String.Encoding.utf8.rawValue)
             
+        
+            
+            var dateData = dataAsNSString! as String
+            
+          
+            
+          
+            
+            
+            
            DispatchQueue.main.async() {
-               self.currentDay = dataAsNSString as! String
+            
+            if dateData.range(of:"RED") != nil{
+                self.currentDay = "Tomorrow is a red day"
+                self.textColor = Color.red
+            }
+            else if dateData.range(of:"BLUE") != nil{
+                self.currentDay = "Tomorrow is a blue day"
+                self.textColor = Color.blue
+            }
+            else if dateData.range(of:"FINALS") != nil{
+                self.currentDay = "Tomorrow you have finals"
+                self.textColor = Color.white
+            }
+            else{
+                self.currentDay = "Tomorrow there is no school"
+                self.textColor = Color.white
+            }
+            
+            
+            
+            //self.currentDay = "now: \(dateText) before: \(beforeDate) after: \(afterDate)"
+            
            }
            
         }
--- Red-or-Blue-WatchOS Extension/ContentView.swift
+++ Red-or-Blue-WatchOS Extension/ContentView.swift
@@ -9,11 +9,15 @@
 import SwiftUI
 
 struct ContentView: View {
+    @ObservedObject var day = GetDay()
     var body: some View {
         
+        
         VStack {
-                    Text("Tomorrow is a red day")
-                               .foregroundColor(.red)
+                    Text("\(day.currentDay)")
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(day.textColor)
+                    .animation(Animation.spring())
                 }
             
         
@@ -23,6 +27,91 @@ struct ContentView: View {
        
     }
 }
+
+
+class GetDay: ObservableObject {
+    @Published var currentDay = ""
+    @Published var textColor = Color.white
+
+    init() {
+        
+        //Calculate date
+        let today = Date()
+        let formatter = ISO8601DateFormatter()
+        let timeFromGMT = -1 * 60 * 60 * 7
+        formatter.timeZone = TimeZone(secondsFromGMT: timeFromGMT)
+        formatter.formatOptions.insert(.withFractionalSeconds)
+        var modifiedDate = Calendar.current.date(byAdding: .day, value: 1, to: today)!
+        
+        
+        let dateText = formatter.string(from: today)
+        
+        var modifiedDateBeforeText = formatter.string(from: modifiedDate)
+        
+        
+        var beforeDate = modifiedDateBeforeText.prefix(10)
+       
+        
+        
+        
+        let url = NSURL(string: "https://www.googleapis.com/calendar/v3/calendars/lgsuhsd.org_tebqf0pqvog3p4s5flsmddg4u8%40group.calendar.google.com/events?&key=AIzaSyB9LL_hsg4zvXzMhUATBCXp92P5Q7LHxe0&timeMin=\(beforeDate)T10:00:00-07:00&singleEvents=true&orderBy=startTime&timeMax=\(beforeDate)T10:01:00-07:00")
+        
+        let task = URLSession.shared.dataTask(with: url! as URL) {(data, response, error) in
+            let dataAsNSString = NSString(data: data!, encoding: String.Encoding.utf8.rawValue)
+            
+        
+            
+            var dateData = dataAsNSString! as String
+            
+          
+            
+          
+            
+            
+            
+           DispatchQueue.main.async() {
+            
+            if dateData.range(of:"RED") != nil{
+                self.currentDay = "Tomorrow is a red day"
+                self.textColor = Color.red
+            }
+            else if dateData.range(of:"BLUE") != nil{
+                self.currentDay = "Tomorrow is a blue day"
+                self.textColor = Color.blue
+            }
+            else if dateData.range(of:"FINALS") != nil{
+                self.currentDay = "Tomorrow you have finals"
+                self.textColor = Color.white
+            }
+            else{
+                self.currentDay = "Tomorrow there is no school"
+                self.textColor = Color.white
+            }
+            
+            
+            
+            //self.currentDay = "now: \(dateText) before: \(beforeDate) after: \(afterDate)"
+            
+           }
+           
+        }
+        task.resume()
+        
+        
+
+        
+        
+        
+        
+        
+        
+    }
+        
+        
+        
+}
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
